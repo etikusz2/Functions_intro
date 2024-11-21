426dd17707f9958698867d870da108230f960e29
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-21 18:43:17 +0200

    palindromes

--- functions.py
+++ functions.py
@@ -3,14 +3,23 @@ def multiply(x, y):
     return result
 
 
-answer = multiply(10.5, 4)
-print(answer)
+def is_palindrome(string):
+    # backwards = string[::-1]
+    # return backwards == string
+    return string[::-1].casefold() == string.casefold()
 
-forty_two = multiply(6, 7)
-print(forty_two)
 
-print()
+def palindrome_sentence(sentence):
+    string = ""
+    for char in sentence:
+        if char.isalnum():
+            string += char
 
-for val in range(1, 5):
-    two_times = multiply(2, val)
-    print(two_times)
+    return string[::-1].casefold() == string.casefold()
+
+
+word = input("please enter a word to check: ")
+if palindrome_sentence(word):
+    print("'{}' is a palindrome".format(word))
+else:
+    print("'{}' is not a palindrome".format(word))
