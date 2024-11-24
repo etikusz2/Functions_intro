b1301dbb7eb988854ace62f1d238e2f83c6cbddb
Birtalan Etele <etele.birtalan@gmail.com>
2024-11-24 17:15:50 +0200

    default parameters values

--- banner.py
+++ banner.py
@@ -1,5 +1,4 @@
-def banner_text(text):
-    screen_width = 50
+def banner_text(text, screen_width = 80):
     if len(text) > screen_width -4:
         raise ValueError("String {0} is larger than specified width {1}"
                          .format(text, screen_width))
@@ -17,14 +16,14 @@ banner_text(" Always look on the bright side of life... ")
 banner_text("If life seems jolly rotten,")
 banner_text(" There's something you've forgotten ")
 banner_text("And that's to laugh and smile and dance and sing,")
-banner_text(" ")
-banner_text("When you/re feeling in the dumps,")
-banner_text("Don't be silly chumps ")
-banner_text("Just purse you lips and whistle - that's the thing! ")
-banner_text("And...always look on the bright side of life... ")
-banner_text("*")
+banner_text(" ", 80)
+banner_text("When you/re feeling in the dumps,", 80)
+banner_text("Don't be silly chumps ", 66)
+banner_text("Just purse you lips and whistle - that's the thing! ", 66)
+banner_text("And...always look on the bright side of life... ", 66)
+banner_text("*", 66)
 
-result = banner_text("Nothing is returned")
+result = banner_text("Nothing is returned", 66)
 print(result)
 
 numbers = [4, 2, 7, 5, 8, 3, 9, 6, 1]
